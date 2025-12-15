3e1aa50f146ac2e8fbf1bc0d3e13100655c06a89
Eric Biagioli <ericbiagioli@gmail.com>
2025-12-14 20:56:27 -0500

    sync

--- enfoque1.py
+++ enfoque1.py
@@ -5,49 +5,10 @@ import numpy as np
 import tkinter as tk
 from types import SimpleNamespace
 
-"""
-Ajusta el tamaño de la imagen `img` para que entre en la ventana `win` y
-la muestra.
-"""
-def show_fitted(win, img):
-    img_h, img_w = img.shape[:2]
-
-    _, _, win_w, win_h = cv2.getWindowImageRect(win)
-
-    scale = min(win_w / img_w, win_h / img_h)
-    new_w = int(img_w * scale)
-    new_h = int(img_h * scale)
-
-    resized = cv2.resize(img, (new_w, new_h))
-    cv2.imshow(win, resized)
-
-
-"""
-Calcula la matriz de cámara y las distorsiones radiales y tangenciales. De
-momento, propongo una aproximación bien simple.
-
-ASUMO QUE el punto principal (esto es: la intersección del eje óptico con
-el sensor) está ubicado exactamente en el centro geométrico del sensor. Esto
-es: (w / 2, h / 2). Esto raramente es así; pero de momento lo asumo.
-@TODO: Analizar si esta suposición afecta a las mediciones. Podría ser que en
-realidad no importe para nuestros fines si el punto principal no es el que
-consideramos.
-
-ASUMO QUE la distancia focal es 0.8 * max(w, h).
-
-ASUMO QUE no hay distorsión (ni radial ni tangencial).
-"""
-def default_camera_matrix(w: int, h: int):
-    focal_length_px = 0.8 * max(w, h)
-    cx = w / 2.0
-    cy = h / 2.0
-    K = np.array([[focal_length_px, 0, cx],
-                  [0, focal_length_px, cy],
-                  [0, 0, 1]], dtype=np.float64)
-
-    distorsion = np.zeros((5, 1), dtype=np.float64)
-
-    return K, distorsion
+from helpers import show_fitted
+from helpers import default_camera_matrix
+from helpers import draw_results
+from helpers import get_distance
 
 
 """
@@ -101,7 +62,6 @@ def detect_markers(img, marker_side_m: float, K, D, frame_prev=None):
     gray = clahe.apply(gray)
     gray = cv2.GaussianBlur(gray, (3,3), 0)
 
-
     ar_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_1000)
     ar_pars = cv2.aruco.DetectorParameters()
 
@@ -125,11 +85,6 @@ def detect_markers(img, marker_side_m: float, K, D, frame_prev=None):
     if ids is None or corners is None or len(ids) == 0:
       return []
 
-    # Refine
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6)
-    stacked = np.vstack(corners).astype(np.float32)
-    cv2.cornerSubPix(gray, stacked, winSize=(5,5), zeroZone=(-1,-1), criteria=criteria)
-
     # ===== Calcular tvec y rvec para cada marcador detectado
 
     # Puntos del marcador, en coordenadas del marcador
@@ -148,11 +103,6 @@ def detect_markers(img, marker_side_m: float, K, D, frame_prev=None):
       ok, rvec, tvec = cv2.solvePnP(object_points, image_points, K, D, flags=cv2.SOLVEPNP_ITERATIVE)
       cv2.solvePnPRefineLM(object_points, image_points, K, D, rvec, tvec)
 
-      #alpha = 0.85
-      #tvec = alpha*tvec_prev + (1-alpha)*tvec
-
-
-
       ret.append({'id': int(marker_id),
                   'corners': corners[i].reshape(-1, 2).tolist(),
                   'rvec':rvec,
@@ -160,19 +110,6 @@ def detect_markers(img, marker_side_m: float, K, D, frame_prev=None):
 
     return ret
 
-
-def draw_results(image, results, K, D, marker_length_m):
-    out = image.copy()
-    for r in results:
-        # borde
-        corners = np.array(r['corners'], dtype=np.float32).reshape(-1, 2)
-        corners_i = corners.astype(int)
-        cv2.polylines(out, [corners_i], True, (0, 255, 0), 2)
-
-        # ejes
-        cv2.drawFrameAxes(out, K, D, r['rvec'], r['tvec'], marker_length_m * 0.5)
-
-    return out
 
 
 """
@@ -191,45 +128,85 @@ Posteriormente, se procede a leer el teclado y enviar la tecla leída al objeto
 de estado.
 """
 
-class Control:
-  def __init__(self, name):
-    self.name = name
-    self.point = None
-    self.capturing = False
+class LastNSeconds:
+  def __init__(self):
     self.captured = []
-    self.starttime = None
+    self.TIME_INTERVAL = 20.0
 
-  def start(self):
-    self.point = None
-    self.capturing = True
-    self.captured = []
-    self.starttime = datetime.now()
-
-  def process(self, p):
-    if self.capturing == True:
-      if(datetime.now() - self.starttime).total_seconds() < 3.0:
-        self.captured.append(p)
-      else:
-        self.capturing = False
-        if len(self.captured) == 0:
-          self.point = None
-        else:
-          arr = np.array(self.captured).reshape(len(self.captured), 3)
-          self.point = np.mean(arr, axis=0).reshape(3,1)
-          self.captured = []
-          self.starttime = None
+  def add(self, p):
+    t = datetime.now()
+    self.captured.append( (t, p))
+    i = 0
+    while i < len(self.captured) and (t - self.captured[i][0]).total_seconds() > self.TIME_INTERVAL:
+      i += 1
+    self.captured = self.captured[i:]
+    #self.filter_outliers()
 
 
-def get_distance(p1, p2):
-  if p1 is None or p2 is None:
-    return None
-  return np.linalg.norm(p1- p2)
+  def filter_outliers(self):
+    points = np.array([p for (_, p) in self.captured])
+    centroid = points.mean(axis = 0)
+    std = points.std(axis = 0)
+    dists = np.linalg.norm(points - centroid, axis=1)
+    mu = dists.mean()
+    sigma = dists.std()
 
+    if sigma == 0:
+        mask = np.ones(len(points), dtype=bool)
+    else:
+        mask = (dists >= mu - 1.0 * sigma) & (dists <= mu + 1.0 * sigma)
+
+    self.captured = points[mask]
+
+
+  def ncaptures(self):
+    return len(self.captured)
+
+  def stats(self):
+    points = np.array([p for (_, p) in self.captured])
+    mean = points.mean(axis = 0)
+    std = points.std(axis = 0)
+    return mean, std
+
+  def ok(self):
+    points = np.array([p for _, p in self.captured])
+    mean = points.mean(axis = 0)
+    std = points.std(axis = 0)
+    ok1 = (len(self.captured) > 9)
+    ok2 = (std < 0.001).all()
+    return (ok1 and ok2), mean, std
+
+
+
+def obj_to_cammera_coords(obj_coords, rvec, tvec):
+  # cv2.Rodrigues transforma una representacion vectorial de una rotacion en
+  # la representación matricial, que llamaremos R.
+  R, _ = cv2.Rodrigues(rvec)
+  return R @ obj_coords.reshape(3,1) + tvec
+
+
+class Writer:
+  def __init__(self, img):
+    self.cx = 50
+    self.cy = 50
+    self.img = img
+
+  def write(self, text):
+    cv2.putText(self.img, text, (self.cx, self.cy), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+    self.cy = self.cy + 30
 
 def process_video(params):
   device = 1
   cap = cv2.VideoCapture(device, cv2.CAP_V4L2)
-  status = dict(m=Control('m'), n=Control('n'))
+
+  stats : Dict[int, LastNSeconds] = {}
+
+  point_m = None
+  point_n = None
+  distance = get_distance(point_m, point_n)
+  capturing_m = False
+  capturing_n = False
+
 
   if not cap.isOpened():
     print("No se pudo abrir la cámara")
@@ -243,10 +220,6 @@ def process_video(params):
   h, w = frame.shape[:2]
   K, D = default_camera_matrix(w, h)
 
-  distance = None
-
-  markers = None
-
   while True:
     ret, frame = cap.read()
 
@@ -259,70 +232,88 @@ def process_video(params):
         print("No se pudo leer el frame de la cámara")
         exit(0)
 
-    markers_prev = markers
-    markers = detect_markers(frame, params.marker_side_m, K, D, markers_prev)
+    markers = detect_markers(frame, params.marker_side_m, K, D)
 
-    for idx, marker in enumerate(markers):
-      punta_del_palito_en_coordenadas_de_camara = None
 
-      # Sistema de coordenadas del marcador: El marcador tiene origen en su centro.
+    for marker in markers:
+      if marker['id'] not in stats:
+        stats[marker['id']] = LastNSeconds()
+
+      # Sistema de coordenadas del marcador:
+      # El marcador tiene origen en su centro.
       # +x: Apunta hacia la derecha en la imagen del marcador.
       # +y: Apunta hacia abajo en la imagen del marcador.
-      # +z: Apunta perpendicular al plano del marcador, hacia la cámara cuando el marcador está frente a la cámara.
+      # +z: Apunta perpendicular al plano del marcador, hacia la cámara cuando
+      #     el marcador está frente a la cámara.
       rvec = marker['rvec']
       tvec = marker['tvec'].reshape(3, 1)
 
-      # cv2.Rodrigues transforma una representacion vectorial de una rotacion en
-      # la representación matricial, que llamaremos R. La necesitaremos para
-      # calcular donde está la punta del palito.
-      R, _ = cv2.Rodrigues(rvec)
+      # palito_aruco es la punta del palito en coordenadas aruco
+      # palito_camera es la punta del palito en coordenadas de camara
+      palito_aruco = np.array([0.0, -0.27, 0.0])
+      palito_camara = obj_to_cammera_coords(palito_aruco, rvec, tvec)
+      stats[marker['id']].add(palito_camara)
 
-      # Definir las posición de la punta del palito en coordenadas del marcador.
-      punta_del_palito_en_coordenadas_aruco = np.array([0.0, -0.27, 0.0])
-      punta_del_palito_en_coordenadas_de_camara = R @ punta_del_palito_en_coordenadas_aruco.reshape(3,1) + tvec
-      centro_del_aruco = tvec
-
-      status['m'].process(punta_del_palito_en_coordenadas_de_camara)
-      status['n'].process(punta_del_palito_en_coordenadas_de_camara)
-
-    distance=get_distance(status['m'].point, status['n'].point)
-
-    ############################################################################
-    # Dibujar los marcadores
     out = draw_results(frame, markers, K, D, params.marker_side_m)
+    distance = get_distance(point_m, point_n)
 
-    # Escribir textos que ayudan a debuguear
-    for idx, marker in enumerate(markers):
-      cv2.putText(out, f"Aruco: {centro_del_aruco}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-      cv2.putText(out, f"Palito: {punta_del_palito_en_coordenadas_de_camara}",   (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+    # Escribir textos
+    w = Writer(out)
+    for marker in markers:
+      m_id = marker['id']
+      rvec = marker['rvec']
+      tvec = marker['tvec'].reshape(3, 1)
+      palito_aruco = np.array([0.0, -0.27, 0.0])
+      palito_camara = obj_to_cammera_coords(palito_aruco, rvec, tvec)
+
+      ncaptures = stats[m_id].ncaptures()
+      avg,std = stats[m_id].stats()
+      w.write(f"Aruco instantaneo [{marker['id']}] = {tvec}")
+      w.write(f"Palito instantaneo [{marker['id']}] = {palito_camara}")
+      w.write(f"ncaptures[{marker['id']}] = {ncaptures}")
+      w.write(f"avg[{marker['id']}] = {avg}")
+      w.write(f"std[{marker['id']}] = {std}")
 
       ## Dibujar la punta del palito
-      p_mark = punta_del_palito_en_coordenadas_aruco.astype(np.float32).reshape(1,1,3)
-      palito_2d, _ = cv2.projectPoints(
-        p_mark,
-        rvec.astype(np.float32),
-        tvec.astype(np.float32),
-        K.astype(np.float32),
-        D.astype(np.float32)
-      )
-
+      p_mark = palito_aruco.astype(np.float32).reshape(1,1,3)
+      palito_2d, _ = cv2.projectPoints( p_mark, rvec.astype(np.float32),
+        tvec.astype(np.float32), K.astype(np.float32), D.astype(np.float32))
       if not np.isnan(palito_2d[0,0]).any():
         px = int(palito_2d[0,0,0])
         py = int(palito_2d[0,0,1])
         cv2.circle(out, (px, py), 5, (0,0,255), -1)
 
+    ok = False
+    if 63 in stats.keys():
+      ok, centroid, deviation = stats[63].ok()
 
-    cv2.putText(out, f"point m: {status['m'].point}", (50, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-    cv2.putText(out, f"point n: {status['n'].point}", (50, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-    cv2.putText(out, f"distance: {distance}", (50, 170), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+    if capturing_m:
+      if ok:
+        capturing_m = False
+        point_m = centroid
+      else:
+        point_m = "capturing"
+
+    if capturing_n:
+      if ok:
+        capturing_n = False
+        point_n = centroid
+      else:
+        point_n = "capturing"
+
+    distance = get_distance(point_m, point_n)
+
+    w.write(f"m = {point_m}")
+    w.write(f"n = {point_n}")
+    w.write(f"dist: {distance}")
 
     show_fitted(params.winname, out)
 
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
-      status['m'].start()
+      capturing_m = True
     elif k == ord('n'):
-      status['n'].start()
+      capturing_n = True
     elif k == ord('q'):
         break
 
